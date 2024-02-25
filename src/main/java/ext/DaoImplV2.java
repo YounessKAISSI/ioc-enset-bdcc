897b1443237155f3e2ab7f6d18048ca99b0111df
Youness KAISSI <ykaissi@outlook.com>
2024-02-25 18:00:36 +0100

    V1

---
+++ src/main/java/ext/DaoImplV2.java
@@ -0,0 +1,15 @@
+package ext;
+
+import dao.IDao;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+@Repository("dao2")
+public class DaoImplV2 implements IDao {
+    @Override
+    public double getData() {
+        System.out.println("Version Web Service");
+        double t = 77;
+        return t;
+    }
+}
